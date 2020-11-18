93003b65548c6783cd7516c91fc20c62f68ad7be
Timo Schmidt <schmidt@timo.email>
2020-11-18 14:36:23 +0100

    bug fixes, missing import lines

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6fb0c25b8bcebbfcdfc6";
+/******/ 	var hotCurrentHash = "6bcf631d30b02255bf0a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
