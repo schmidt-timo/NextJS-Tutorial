b305a83c0efa20483d439f89ec0792a26d397a46
Timo Schmidt <schmidt@timo.email>
2020-11-18 14:26:44 +0100

    markdown also rendered

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "24b80166be5532d1625f";
+/******/ 	var hotCurrentHash = "6fb0c25b8bcebbfcdfc6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
