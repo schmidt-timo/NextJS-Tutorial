f79c9452c14fff4dbe0bfee7c0c8103909d94436
Timo Schmidt <schmidt@timo.email>
2020-11-18 15:35:54 +0100

    add new post

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "02c6aae9f2a8e1e7293b";
+/******/ 	var hotCurrentHash = "ccb1cbebc36efca4b34b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
